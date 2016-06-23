cf2e00ecc492dcdea87a1a578e97f180aafb8283
Luis de Jesus Martin Castillo <luismartin.925@gmail.com>
2016-06-23 17:50:58 -0500

    Initial Commit

---
+++ Lab04/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Lab04
+//
+//  Created by Luis de Jesus Martin Castillo on 23/06/16.
+//  Copyright © 2016 Luis de Jesus Martin Castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Lab04/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab04
+//
+//  Created by Luis de Jesus Martin Castillo on 23/06/16.
+//  Copyright © 2016 Luis de Jesus Martin Castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
